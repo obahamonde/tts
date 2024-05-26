66cf0301464a0ffc9baf7887231c0c87356a3662
obahamonde <oscarmartinbahamondemunoz@gmail.com>
2024-05-26 22:22:43 +0000

    fc

--- qvoice/__init__.py
+++ qvoice/__init__.py
@@ -1,5 +1,6 @@
 import io
-from typing import Literal, TypeAlias
+from functools import wraps
+from typing import Literal, TypeAlias, TypeVar, ParamSpec,Callable, Awaitable
 import aiofiles
 import pytube  # pylint: disable=E0401  # type: ignore
 from cachetools import LRUCache, cached  # type: ignore # pylint: disable=import-error
@@ -15,83 +16,94 @@ Lang: TypeAlias = Literal["en", "es"]
 Gender: TypeAlias = Literal["female", "male"]
 DEVICE = "cpu"
 
+R = TypeVar("R")
+P = ParamSpec("P")
+
+def asyncify(func:Callable[P, R]) -> Callable[P, Awaitable[R]]:
+	@wraps(func)
+	async def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+		return func(*args, **kwargs)
+	return wrapper
+
+
+
 @cached(cache)  # type: ignore
 def load_model():
-    return TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(DEVICE)  # type: ignore
+	return TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(DEVICE)  # type: ignore
 
 
 tts = load_model()
 
 def download_audio(url: str) -> io.BytesIO:
-    yt = pytube.YouTube(url)
-    stream = yt.streams.filter(only_audio=True).first()  # type: ignore
-    audio_file = io.BytesIO()
-    stream.stream_to_buffer(audio_file)  # type: ignore
-    audio_file.seek(0)
-    audio = AudioSegment.from_file(audio_file, format="mp4")  # type: ignore # pylint: disable=E0602
-    wav_file = io.BytesIO()
-    audio.export(wav_file, format="wav")  # type: ignore
-    wav_file.seek(0)
-    return wav_file
+	yt = pytube.YouTube(url)
+	stream = yt.streams.filter(only_audio=True).first()  # type: ignore
+	audio_file = io.BytesIO()
+	stream.stream_to_buffer(audio_file)  # type: ignore
+	audio_file.seek(0)
+	audio = AudioSegment.from_file(audio_file, format="mp4")  # type: ignore # pylint: disable=E0602
+	wav_file = io.BytesIO()
+	audio.export(wav_file, format="wav")  # type: ignore
+	wav_file.seek(0)
+	return wav_file
 
-
+@asyncify
 def generate_audio(text: str, lang: Lang, speaker_wav: str):
-    output_audio = io.BytesIO()
-    tts.tts_to_file(  # type: ignore
-        text=text,
-        file_path=output_audio,  # type: ignore
-        speaker_wav=speaker_wav,
-        split_sentences=True,
-        emotion="cheerful",
-        language=lang,
-    )
-    output_audio.seek(0)
-    return output_audio
+	output_audio = io.BytesIO()
+	tts.tts_to_file(  # type: ignore
+		text=text,
+		file_path=output_audio,  # type: ignore
+		speaker_wav=speaker_wav,
+		split_sentences=True,
+		emotion="cheerful",
+		language=lang,
+	)
+	output_audio.seek(0)
+	return output_audio
 
 
 def create_app():
-    app = FastAPI(
-        title="QVoice",
-        description="A simple API to generate voice from text",
-        version="0.1.0",
-    )
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+	app = FastAPI(
+		title="QVoice",
+		description="A simple API to generate voice from text",
+		version="0.1.0",
+	)
+	app.add_middleware(
+		CORSMiddleware,
+		allow_origins=["*"],
+		allow_methods=["*"],
+		allow_headers=["*"],
+	)
 
-    @app.get("/generate")
-    def _(text: str, lang: Lang, gender: Gender = "female"):
-        return StreamingResponse(
-            generate_audio(text, lang, f"/app/samples/{gender}_{lang}.wav"),
-            media_type="audio/wav",
-        )
+	@app.get("/generate")
+	async def _(text: str, lang: Lang, gender: Gender = "female"):
+		return StreamingResponse(
+			await generate_audio(text, lang, f"/app/samples/{gender}_{lang}.wav"),
+			media_type="audio/wav",
+		)
 
-    @app.post("/generate")
-    async def _(text: str, lang: Lang, name: str, file: UploadFile = File(...)):
-        raw_audio = await file.read()
-        async with aiofiles.open(f"/app/samples/{name}.wav", "wb") as f:
-            await f.write(raw_audio)
+	@app.post("/generate")
+	async def _(text: str, lang: Lang, name: str, file: UploadFile = File(...)):
+		raw_audio = await file.read()
+		async with aiofiles.open(f"/app/samples/{name}.wav", "wb") as f:
+			await f.write(raw_audio)
 
-        stream = io.BytesIO()
+		stream = io.BytesIO()
 
-        tts.tts_to_file(  # type: ignore
-            text=text,
-            file_path=stream,  # type: ignore
-            speaker_wav=f"/app/samples/{name}.wav",
-            split_sentences=True,
-            emotion="cheerful",
-            language=lang,
-        )
+		tts.tts_to_file(  # type: ignore
+			text=text,
+			file_path=stream,  # type: ignore
+			speaker_wav=f"/app/samples/{name}.wav",
+			split_sentences=True,
+			emotion="cheerful",
+			language=lang,
+		)
 
-        stream.seek(0)
+		stream.seek(0)
 
-        return StreamingResponse(
-            stream,
-            media_type="audio/wav",
-            headers={"Content-Disposition": f'attachment; filename="{name}.wav"'},
-        )
+		return StreamingResponse(
+			stream,
+			media_type="audio/wav",
+			headers={"Content-Disposition": f'attachment; filename="{name}.wav"'},
+		)
 
-    return app
+	return app
